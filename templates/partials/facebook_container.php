6609da3cd0c7800dfbcc265365caa4a9a05cf401
Alexander Gruzov <gruz0.mail@gmail.com>
2016-05-02 12:07:44 +0300

    Remove unused Facebook template partials

--- templates/partials/facebook_container.php
+++
@@ -1,1 +0,0 @@
-<div class="fb-page" data-href="%s" data-width="%s" width="%s" data-height="%s" height="%s" data-hide-cover="%s" data-show-facepile="%s" data-adapt-container-width="%s" data-small-header="%s" data-tabs="%s"></div>
