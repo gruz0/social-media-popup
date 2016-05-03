c3130915096997b57c71136c207862449f9b0379
Alexander Gruzov <gruz0.mail@gmail.com>
2016-05-03 13:46:19 +0300

    Activate mobile mode again

--- social-community-popup.class.php
+++ social-community-popup.class.php
@@ -2920,7 +2920,7 @@ class Social_Community_Popup {
 		}
 
 		// Отключаем работу плагина на мобильных устройствах
-		// if ( wp_is_mobile() && get_scp_option( 'setting_show_on_mobile_devices' ) === '0' ) return;
+		if ( wp_is_mobile() && get_scp_option( 'setting_show_on_mobile_devices' ) === '0' ) return;
 
 		$debug_mode                                       = ( (int) $scp_options[ $scp_prefix . 'setting_debug_mode' ] ) == 1;
 
