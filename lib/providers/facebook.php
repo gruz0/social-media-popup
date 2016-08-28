02045a86fb3953f0ce7df735982a57c1bec3688b
Alexander Gruzov <gruz0.mail@gmail.com>
2016-08-28 20:49:19 +0300

    Refactor SCP_Facebook_Provider
    * Split Facebook functions to multiple
    * Fix bug if Facebook already initialized by Theme or another plugins
    
    	modified:   lib/providers/facebook.php

--- lib/providers/facebook.php
+++ lib/providers/facebook.php
@@ -1,5 +1,17 @@
 <?php
+/**
+ * Facebook Template
+ *
+ * @package    Social_Media_Popup
+ * @subpackage SCP_Template
+ * @author     Alexander Gruzov
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link       https://github.com/gruz0/social-media-popup
+ */
 
+/**
+ * SCP_Facebook_Provider
+ */
 class SCP_Facebook_Provider extends SCP_Provider {
 	/**
 	 * Return widget is active
@@ -9,7 +21,7 @@ class SCP_Facebook_Provider extends SCP_Provider {
 	 * @return boolean
 	 */
 	public static function is_active() {
-		return ( self::$options[ self::$prefix . 'setting_use_facebook' ] === '1' );
+		return ( '1' === self::$options[ self::$prefix . 'setting_use_facebook' ] );
 	}
 
 	/**
@@ -21,10 +33,10 @@ class SCP_Facebook_Provider extends SCP_Provider {
 	 */
 	public static function options() {
 		return array(
-			'tab_caption' => esc_attr( self::$options[ self::$prefix . 'setting_facebook_tab_caption'] ),
+			'tab_caption' => esc_attr( self::$options[ self::$prefix . 'setting_facebook_tab_caption' ] ),
 			'css_class'   => 'facebook-tab',
 			'icon'        => 'fa-facebook',
-			'url'         => self::$options[ self::$prefix . 'setting_facebook_page_url' ]
+			'url'         => esc_attr( self::$options[ self::$prefix . 'setting_facebook_page_url' ] ),
 		);
 	}
 
@@ -35,21 +47,51 @@ class SCP_Facebook_Provider extends SCP_Provider {
 	 * @uses SCP_Template()->use_events_tracking()
 	 * @uses SCP_Template()->push_social_media_trigger_to_google_analytics()
 	 * @uses SCP_Template()->push_social_network_and_action_to_google_analytics()
+	 * @uses SCP_Facebook_Provider::prepare_facebook_widget()
+	 * @uses SCP_Facebook_Provider::prepare_facebook_content()
+	 * @uses SCP_Facebook_Provider::prepare_facebook_events()
+	 *
+	 * @uses_by SCP_Facebook_Provider::container();
 	 *
 	 * @since 0.7.5
 	 *
 	 * @return string
 	 */
 	public static function container() {
-		$close_window_after_join = ( (int) self::$options[ self::$prefix . 'setting_facebook_close_window_after_join' ] ) == 1;
+		$close_window_after_join = ( 1 === absint( self::$options[ self::$prefix .  'setting_facebook_close_window_after_join' ] ) );
 
 		$content = '<div class="box">';
 
-		if ( self::$options[ self::$prefix . 'setting_facebook_show_description' ] === '1' ) {
+		// FIXME: Should be refactored with self::show_description() and move condition to it
+		if ( '1' === self::$options[ self::$prefix . 'setting_facebook_show_description' ] ) {
 			$content .= '<p class="widget-description"><b>' . self::$options[ self::$prefix . 'setting_facebook_description' ] . '</b></p>';
 		}
 
-		$content .= '<div class="fb-page" '
+		$content .= self::prepare_facebook_widget();
+
+		$content .= '</div>'; // .box
+
+		$content .= '<script>function scp_prependFacebook($) {';
+
+		$content .= self::prepare_facebook_content();
+		$content .= self::prepare_facebook_events();
+
+		$content .= '}</script>'; // scp_prependFacebook()
+
+		return $content;
+	}
+
+	/**
+	 * Returns Facebook widget code
+	 *
+	 * @since 0.7.5
+	 *
+	 * @used_by SCP_Facebook_Provider::container();
+	 *
+	 * @return string
+	 */
+	private static function prepare_facebook_widget() {
+		return '<div class="fb-page" '
 			. 'data-href="' .                  esc_attr( self::$options[ self::$prefix . 'setting_facebook_page_url' ] ) . '" '
 			. 'data-width="' .                 esc_attr( self::$options[ self::$prefix . 'setting_facebook_width' ] ) . '" '
 			. 'width="' .                      esc_attr( self::$options[ self::$prefix . 'setting_facebook_width' ] ) . '" '
@@ -61,37 +103,19 @@ class SCP_Facebook_Provider extends SCP_Provider {
 			. 'data-small-header="' .          esc_attr( scp_to_bool( self::$options[ self::$prefix . 'setting_facebook_use_small_header' ] ) ) . '" '
 			. 'data-tabs="' .                  esc_attr( self::$options[ self::$prefix . 'setting_facebook_tabs' ] ) . '" '
 			. '></div>';
+	}
 
-		$content .= '</div>';
-
-		$content .= '<script>function scp_prependFacebook($) {';
-
-		$prepend_facebook = '<div id="fb-root"></div>'
-			. '<script>';
-
-		// Формирует колбэк для обработки событий при закрытии окна, подписке или отписке от группы
-		$prepend_facebook .= 'var scp_facebook_page_like_or_unlike_callback = function(url, html_element) {';
-
-		if ( (int) self::$options[ self::$prefix . 'setting_facebook_close_window_after_join' ] ) {
-			$prepend_facebook .= 'scp_destroyPlugin(scp.showWindowAfterReturningNDays);';
-		}
-
-		if ( self::$template->use_events_tracking() && ( ( (int) self::$options[ self::$prefix . 'tracking_use_facebook' ] ) == 1 ) ) {
-			$prepend_facebook .= self::$template->push_social_media_trigger_to_google_analytics( esc_attr( self::$options[ self::$prefix . 'tracking_facebook_event' ] ) );
-			$prepend_facebook .= self::$template->push_social_network_and_action_to_google_analytics( 'SMP Facebook', 'Subscribe' );
-		}
-
-		$prepend_facebook .= '};
-
-		window.fbAsyncInit = function() {
-			FB.init({
-				appId  : "' . esc_attr( self::$options[ self::$prefix . 'setting_facebook_application_id' ] ) . '",
-				xfbml  : true,
-				version: "v2.5"
-			});
-
-			FB.Event.subscribe("edge.create", scp_facebook_page_like_or_unlike_callback);
-		};';
+	/**
+	 * Prepare Facebook container
+	 *
+	 * @since 0.7.5
+	 *
+	 * @used_by SCP_Facebook_Provider::container();
+	 *
+	 * @return string
+	 */
+	private static function prepare_facebook_content() {
+		$prepend_facebook = '<div id="fb-root"></div><script>';
 
 		// Подключаем сам Facebook
 		$prepend_facebook .= '(function(d, s, id) {'
@@ -104,18 +128,70 @@ class SCP_Facebook_Provider extends SCP_Provider {
 			. '}(document, "script", "facebook-jssdk"));</script>';
 
 		// Удаляем переносы строк, иначе jQuery ниже не отработает
-		$prepend_facebook = str_replace("\n", '', $prepend_facebook);
+		$prepend_facebook = str_replace( "\n", '', $prepend_facebook );
 
 		// Переводим код в сущности
 		$prepend_facebook = htmlspecialchars( $prepend_facebook, ENT_QUOTES );
 
-		$content .= 'if ($("#fb-root").length == 0) {
+		return 'if ($("#fb-root").length == 0) {
 			$("body").prepend($("<div/>").html("' . esc_attr( $prepend_facebook ) . '").text());
 		}';
+	}
 
-		$content .= '}</script>'; // scp_prependFacebook
+	/**
+	 * Prepare Facebook events HTML code
+	 *
+	 * @since 0.7.5
+	 *
+	 * @used_by SCP_Facebook_Provider::container();
+	 *
+	 * @return string
+	 */
+	private static function prepare_facebook_events() {
+		$facebook_events = '<script>';
 
-		return $content;
+		// Формирует колбэк для обработки событий при закрытии окна, подписке или отписке от группы
+		$facebook_events .= 'var scp_facebook_page_like_or_unlike_callback = function(url, html_element) {';
+
+		if ( (int) self::$options[ self::$prefix . 'setting_facebook_close_window_after_join' ] ) {
+			$facebook_events .= 'scp_destroyPlugin(scp.showWindowAfterReturningNDays);';
+		}
+
+		// FIXME: Should be refactored with self::use_widget() and move second condition to it
+		if ( self::$template->use_events_tracking() && ( 1 === absint( self::$options[ self::$prefix . 'tracking_use_facebook' ] ) ) ) {
+			$facebook_events .= self::$template->push_social_media_trigger_to_google_analytics(
+				esc_attr( self::$options[ self::$prefix . 'tracking_facebook_event' ] )
+			);
+			$facebook_events .= self::$template->push_social_network_and_action_to_google_analytics( 'SMP Facebook', 'Subscribe' );
+		}
+
+		$facebook_events .= '};';
+
+		// $facebook_events = '<script>
+		$facebook_events .= '
+		if ( typeof window.fbAsyncInit == "undefined" ) {
+			window.fbAsyncInit = function() {
+				FB.init({
+					appId  : "' . esc_attr( self::$options[ self::$prefix . 'setting_facebook_application_id' ] ) . '",
+					xfbml  : true,
+					version: "v2.5"
+				});
+
+				FB.Event.subscribe("edge.create", scp_facebook_page_like_or_unlike_callback);
+			};
+		} else {
+			smp_facebookInterval = setInterval(function() {
+				if ( typeof FB === "object" ) {
+					FB.Event.subscribe("edge.create", scp_facebook_page_like_or_unlike_callback);
+					clearInterval(smp_facebookInterval);
+				}
+			}, 1000);
+		}
+		</script>';
+
+		$facebook_events = htmlspecialchars( $facebook_events, ENT_QUOTES );
+
+		return '$("#fb-root").prepend($("<div/>").html("' . esc_attr( $facebook_events ) . '").text());';
 	}
 }
 
