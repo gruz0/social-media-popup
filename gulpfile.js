d12916af403074bb32f32137e2493195a6a3f36f
Alexander Gruzov <gruz0.mail@gmail.com>
2016-09-10 20:06:50 +0300

    Ignore minified CSS files from Gulp minifier
    modified:   gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,11 @@ var gulp     = require('gulp');
 var cleanCSS = require('gulp-clean-css');
 var rename   = require('gulp-rename');
 
+//setup minify task
+var cssMinifyLocation = ['css/*.css', '!css/*.min.css'];
+
 gulp.task('minify-css', function() {
-    return gulp.src('css/*.css')
+    return gulp.src(cssMinifyLocation)
         .pipe(cleanCSS({debug: true}, function(details) {
             console.log(details.name + ': ' + details.stats.originalSize);
             console.log(details.name + ': ' + details.stats.minifiedSize);
