9be22cae4e4e1d0bb9c8feef34a2419d5e0d9337
Alexander Gruzov <gruz0.mail@gmail.com>
2017-06-18 12:09:03 +0300

    Fix bug when VKontakte widget was not showing in Firefox
    modified:   includes/providers/vkontakte.php

--- includes/providers/vkontakte.php
+++ includes/providers/vkontakte.php
@@ -80,63 +80,73 @@ class SCP_VK_Provider extends SCP_Provider {
 			<div id="scp_vk_groups" style="height:250px !important;"></div>
 			<script type="text/javascript">
 				var vk_initialized = 0;
+				var scp_vk_container_height  = parseInt("' . self::get_option_as_escaped_string( 'setting_vkontakte_height' ) . '");
 
 				function initialize_VK_Widgets() {
-					if ((typeof(VK) === "undefined") || !vk_initialized) {
-						jQuery.getScript( "//vk.com/js/api/openapi.js?115", function(data, textStatus, jqxhr) {
-							VK.init({apiId: ' . $application_id . ' });
-
-							VK.Observer.subscribe("widgets.groups.joined", function f() {';
-
-								if ( self::get_option_as_boolean( 'setting_vkontakte_close_window_after_join' ) ) {
-									$content .= 'scp_destroyPlugin(scp.showWindowAfterReturningNDays);';
-								}
-
-								if ( self::$template->use_events_tracking() && self::get_option_as_boolean( 'tracking_use_vkontakte' ) ) {
-									$content .= self::$template->push_social_media_trigger_to_google_analytics( self::get_option_as_escaped_string( 'tracking_vkontakte_subscribe_event' ) );
-									$content .= self::$template->push_social_network_and_action_to_google_analytics( 'SMP VK', 'Subscribe' );
-								}
-
-							$content .= '});
-
-							VK.Observer.subscribe("widgets.groups.leaved", function f() {';
-
-								if ( self::$template->use_events_tracking() && self::get_option_as_boolean( 'tracking_use_vkontakte' ) ) {
-									$content .= self::$template->push_social_media_trigger_to_google_analytics( self::get_option_as_escaped_string( 'tracking_vkontakte_unsubscribe_event' ) );
-									$content .= self::$template->push_social_network_and_action_to_google_analytics( 'SMP VK', 'Unsubscribe' );
-								}
-
-							$content .= '});
-
-							VK.Widgets.Group("scp_vk_groups", {
-								mode: '    . self::get_option_as_escaped_string( 'setting_vkontakte_layout' ) . ',
-								width: "'  . self::get_option_as_integer( 'setting_vkontakte_width' ) . '",
-								height: "' . self::get_option_as_integer( 'setting_vkontakte_height' ) . '",
-								color1: "' . self::get_option_as_escaped_string( 'setting_vkontakte_color_background' ) . '",
-								color2: "' . self::get_option_as_escaped_string( 'setting_vkontakte_color_text' ) . '",
-								color3: "' . self::get_option_as_escaped_string( 'setting_vkontakte_color_button' ) . '"
-							}, ' . $page_or_group_id . ');
-							vk_initialized = 1;
-						});
+					if (jQuery("#scp_vk_groups iframe").length && jQuery("#scp_vk_groups iframe").height() < scp_vk_container_height) {
+						jQuery("#scp_vk_groups iframe").height(scp_vk_container_height);
 					}
+
+					if (vk_initialized) return;
+
+					jQuery.getScript( "//vk.com/js/api/openapi.js?115", function(data, textStatus, jqxhr) {
+						VK.init({apiId: ' . $application_id . ' });
+
+						VK.Observer.subscribe("widgets.groups.joined", function f() {';
+
+							if ( self::get_option_as_boolean( 'setting_vkontakte_close_window_after_join' ) ) {
+								$content .= 'scp_destroyPlugin(scp.showWindowAfterReturningNDays);';
+							}
+
+							if ( self::$template->use_events_tracking() && self::get_option_as_boolean( 'tracking_use_vkontakte' ) ) {
+								$content .= self::$template->push_social_media_trigger_to_google_analytics( self::get_option_as_escaped_string( 'tracking_vkontakte_subscribe_event' ) );
+								$content .= self::$template->push_social_network_and_action_to_google_analytics( 'SMP VK', 'Subscribe' );
+							}
+
+						$content .= '});
+
+						VK.Observer.subscribe("widgets.groups.leaved", function f() {';
+
+							if ( self::$template->use_events_tracking() && self::get_option_as_boolean( 'tracking_use_vkontakte' ) ) {
+								$content .= self::$template->push_social_media_trigger_to_google_analytics( self::get_option_as_escaped_string( 'tracking_vkontakte_unsubscribe_event' ) );
+								$content .= self::$template->push_social_network_and_action_to_google_analytics( 'SMP VK', 'Unsubscribe' );
+							}
+
+						$content .= '});
+
+						VK.Widgets.Group("scp_vk_groups", {
+							mode: '    . self::get_option_as_escaped_string( 'setting_vkontakte_layout' ) . ',
+							width: "'  . self::get_option_as_integer( 'setting_vkontakte_width' ) . '",
+							height: "' . self::get_option_as_integer( 'setting_vkontakte_height' ) . '",
+							color1: "' . self::get_option_as_escaped_string( 'setting_vkontakte_color_background' ) . '",
+							color2: "' . self::get_option_as_escaped_string( 'setting_vkontakte_color_text' ) . '",
+							color3: "' . self::get_option_as_escaped_string( 'setting_vkontakte_color_button' ) . '"
+						}, ' . $page_or_group_id . ');
+
+						vk_initialized = 1;
+					});
 				}
 
 				function scp_prependVK($) {
-					timeout = ' . $delay_before_render . ';
-
-					$tabs   = $("' . self::$tabs_id . '");
 					$vk_tab = $("' . self::$tabs_id . ' .vk-tab");
 
-					if ($vk_tab.length && parseInt($vk_tab.data("index")) == 1) {
-						setTimeout("initialize_VK_Widgets();", timeout);
-					} else if ($tabs.length == 0) {
-						setTimeout("initialize_VK_Widgets();", timeout);
-					}
+					var scp_vk_interval = setInterval(function() {
+						var container_height_is_too_small = jQuery("#scp_vk_groups iframe").height() < scp_vk_container_height;
+
+						if (jQuery("#scp_vk_groups iframe").length > 0 || container_height_is_too_small) {
+							jQuery("#scp_vk_groups iframe").height(scp_vk_container_height);
+
+							container_height_is_too_small = jQuery("#scp_vk_groups iframe").height() < scp_vk_container_height;
+							if (!container_height_is_too_small) {
+								setTimeout(function() { clearInterval(scp_vk_interval); }, 3000);
+							}
+						}
+					}, 1000);
+
+					initialize_VK_Widgets();
 
 					$vk_tab.on("click", function() {
-						if ((typeof(VK) !== "undefined") && !vk_initialized) {
-							setTimeout("initialize_VK_Widgets();", timeout);
-						}
+						initialize_VK_Widgets();
 					});
 				}
 			</script>';
