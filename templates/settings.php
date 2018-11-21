84271f8eb345cea48a689543a0ba931160342f50
Alexander Kadyrov <gruz0.mail@gmail.com>
2018-11-21 08:55:32 +0300

    Refactor settings.php with phpcs suggestions

--- templates/settings.php
+++ templates/settings.php
@@ -11,11 +11,7 @@
 defined( 'ABSPATH' ) or exit;
 
 $available_tabs = array( 'general', 'view', 'events', 'management', 'view-mobile', 'tracking' );
-
-$tab = 'general';
-if ( ! empty( $_GET['tab'] ) && in_array( wp_unslash( $_GET['tab'] ), $available_tabs, true ) ) {
-	$tab = sanitize_text_field( wp_unslash( $_GET['tab'] ) );
-}
+$tab            = smp_validate_and_sanitize_tab( $available_tabs );
 
 $subtab = isset( $_GET['section'] ) ? sanitize_text_field( wp_unslash( $_GET['section'] ) ) : '';
 if ( ! empty( $subtab ) ) {
@@ -24,8 +20,10 @@ if ( ! empty( $subtab ) ) {
 ?>
 
 <div class="wrap social-community-popup-settings">
-	<h1><?php _e( 'Social Media Popup Options', L10N_SCP_PREFIX ); ?></h1>
-	<?php echo scp_settings_tabs(); ?>
+	<h1><?php esc_attr_e( 'Social Media Popup Options', L10N_SCP_PREFIX ); ?></h1>
+
+	<?php scp_settings_tabs(); ?>
+
 	<form method="post" action="options.php">
 		<?php wp_nonce_field( 'scp-update-options' ); ?>
 		<?php settings_fields( SMP_PREFIX . '-group-' . $tab ); ?>
@@ -51,7 +49,9 @@ function scp_settings_tabs() {
 	$tabs['tracking']    = __( 'Tracking', L10N_SCP_PREFIX );
 	$tabs['management']  = __( 'Management', L10N_SCP_PREFIX );
 
-	echo '<h2 class="nav-tab-wrapper">';
+	$tab_template = '<a class="nav-tab %s" href="?page=' . SMP_PREFIX . '&tab=%s">%s</a>';
+
+	$content = '<h2 class="nav-tab-wrapper">';
 	foreach ( $tabs as $tab_key => $tab_caption ) {
 		$active = $current_tab === $tab_key ? 'nav-tab-active' : '';
 
@@ -61,16 +61,25 @@ function scp_settings_tabs() {
 				break;
 		}
 
-		echo '<a class="nav-tab ' . $active . '" href="?page=' . SMP_PREFIX . '&tab=' . $tab_key . '">' . $tab_caption . '</a>';
+		// phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
+		$content .= sprintf(
+			$tab_template,
+			esc_attr( $active ),
+			esc_attr( $tab_key ),
+			esc_attr( $tab_caption )
+		);
+		// phpcs:enable WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
 
-	echo '</h2>';
+	$content .= '</h2>';
 
 	switch ( $current_tab ) {
 		case 'tracking':
-			smp_tracking_menu( $current_tab );
+			$content .= smp_tracking_menu( $current_tab );
 			break;
 	}
+
+	echo $content; // WPCS: XSS OK.
 }
 
 /**
@@ -93,11 +102,22 @@ function smp_tracking_menu( $current_tab ) {
 
 	$subtab_template = '<a class="nav-tab %s" href="?page=' . SMP_PREFIX . '&tab=%s&section=%s">%s</a>';
 
-	echo '<h3 class="nav-tab-wrapper">';
+	$content = '<h3 class="nav-tab-wrapper">';
 	foreach ( $subtabs as $tab_key => $tab_caption ) {
 		$active = $current_subtab === $tab_key ? 'nav-tab-active' : '';
-		echo sprintf( $subtab_template, $active, $current_tab, $tab_key, $tab_caption );
+
+		// phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
+		$content .= sprintf(
+			$subtab_template,
+			esc_attr( $active ),
+			esc_attr( $current_tab ),
+			esc_attr( $tab_key ),
+			esc_attr( $tab_caption )
+		);
+		// phpcs:enable WordPress.Security.EscapeOutput.OutputNotEscaped
 	}
 
-	echo '</h3>';
+	$content .= '</h3>';
+
+	return $content; // WPCS: XSS OK.
 }
