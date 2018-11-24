90f73f5e91cb43ced96544e1d041168b7ffee535
Alexander Kadyrov <gruz0.mail@gmail.com>
2018-11-24 18:05:33 +0300

    Refactor settings/settings-googleplus.php with phpcs suggestions

--- templates/settings/settings-googleplus.php
+++ templates/settings/settings-googleplus.php
@@ -1,6 +1,19 @@
-<?php defined( 'ABSPATH' ) or exit; ?>
+<?php
+/**
+ * Google Plus Settings
+ *
+ * @package Social_Media_Popup
+ * @author  Alexander Kadyrov
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link    https://github.com/gruz0/social-media-popup
+ */
+
+defined( 'ABSPATH' ) or exit;
+?>
+
 <div class="wrap social-community-popup-settings">
-	<h2><?php _e( 'Google+ Options', L10N_SCP_PREFIX ); ?></h2>
+	<h2><?php esc_attr_e( 'Google+ Options', L10N_SCP_PREFIX ); ?></h2>
+
 	<form method="post" action="options.php">
 		<?php wp_nonce_field( 'scp-update-googleplus-options' ); ?>
 		<?php settings_fields( SMP_PREFIX . '-group-googleplus' ); ?>
@@ -8,5 +21,5 @@
 		<?php do_settings_sections( SMP_PREFIX . '_googleplus_options' ); ?>
 		<?php submit_button(); ?>
 	</form>
-	<?php require( sprintf( "%s/../copyright.php", dirname( __FILE__ ) ) ); ?>
+	<?php require( sprintf( '%s/../copyright.php', dirname( __FILE__ ) ) ); ?>
 </div>
