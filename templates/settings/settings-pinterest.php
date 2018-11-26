a8740cdee593bdd47f5d35d3868b079818f21a69
Alexander Kadyrov <gruz0.mail@gmail.com>
2018-11-26 17:06:40 +0300

    Refactor settings/settings-pinterest.php with phpcs suggestions

--- templates/settings/settings-pinterest.php
+++ templates/settings/settings-pinterest.php
@@ -1,6 +1,19 @@
-<?php defined( 'ABSPATH' ) or exit; ?>
+<?php
+/**
+ * Pinterest Settings
+ *
+ * @package Social_Media_Popup
+ * @author  Alexander Kadyrov
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link    https://github.com/gruz0/social-media-popup
+ */
+
+defined( 'ABSPATH' ) or exit;
+?>
+
 <div class="wrap social-community-popup-settings">
-	<h2><?php _e( 'Pinterest Options', L10N_SCP_PREFIX ); ?></h2>
+	<h2><?php esc_attr_e( 'Pinterest Options', L10N_SCP_PREFIX ); ?></h2>
+
 	<form method="post" action="options.php">
 		<?php wp_nonce_field( 'scp-update-pinterest-options' ); ?>
 		<?php settings_fields( SMP_PREFIX . '-group-pinterest' ); ?>
@@ -8,5 +21,5 @@
 		<?php do_settings_sections( SMP_PREFIX . '_pinterest_options' ); ?>
 		<?php submit_button(); ?>
 	</form>
-	<?php require( sprintf( "%s/../copyright.php", dirname( __FILE__ ) ) ); ?>
+	<?php require( sprintf( '%s/../copyright.php', dirname( __FILE__ ) ) ); ?>
 </div>
