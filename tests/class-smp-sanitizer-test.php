f7b77eb8ba24b49ec02007babd3bdaf72d0fba54
Alexander Kadyrov <gruz0.mail@gmail.com>
2019-02-20 23:36:12 +0300

    Split testCanBeSanitizedSectionCommon() to single option tests

--- tests/class-smp-sanitizer-test.php
+++ tests/class-smp-sanitizer-test.php
@@ -11,27 +11,78 @@ include '../../../wp-load.php';
  * SMP_Sanitizer Test
  */
 final class SMP_Sanitizer_Test extends TestCase {
+	const SECTION_COMMON_GENERAL = SMP_PREFIX . '-section-common';
+
 	/**
-	 * Sanitize General > Common section
+	 * Sanitize setting_debug_mode
 	 */
-	public function testCanBeSanitizedSectionCommon(): void {
-		$values = array(
-			'setting_debug_mode'                       => 2,
-			'setting_tabs_order'                       => "  vkontakte,facebook\"<a href='' />\", gooGleplus, twitter, ODNOklassniki , vk , fac3book, twitter",
-			'setting_close_popup_by_clicking_anywhere' => 3,
-			'setting_close_popup_when_esc_pressed'     => 4,
-			'setting_show_on_mobile_devices'           => 5,
-			'setting_show_admin_bar_menu'              => 6,
-		);
+	public function testCanBeSanitizedSettingDebugMode(): void {
+		$key      = 'setting_debug_mode';
+		$value    = 2;
+		$expected = 1;
 
-		$result = SMP_Sanitizer::sanitize( SMP_PREFIX . '-section-common', $values );
+		$result = SMP_Sanitizer::sanitize( self::SECTION_COMMON_GENERAL, array( $key => $value ) );
+		$this->assertEquals( $expected, $result[ $key ] );
+	}
 
-		$this->assertEquals( 1, $result['setting_debug_mode'] );
-		$this->assertEquals( 'vkontakte,googleplus,twitter,odnoklassniki', $result['setting_tabs_order'] );
-		$this->assertEquals( 1, $result['setting_close_popup_by_clicking_anywhere'] );
-		$this->assertEquals( 1, $result['setting_close_popup_when_esc_pressed'] );
-		$this->assertEquals( 1, $result['setting_show_on_mobile_devices'] );
-		$this->assertEquals( 1, $result['setting_show_admin_bar_menu'] );
+	/**
+	 * Sanitize setting_tabs_order
+	 */
+	public function testCanBeSanitizedSettingTabsOrder(): void {
+		$key      = 'setting_tabs_order';
+		$value    = "  vkontakte,facebook\"<a href                = '' />\", gooGleplus, twitter, ODNOklassniki , vk , fac3book, twitter";
+		$expected = 'vkontakte,googleplus,twitter,odnoklassniki';
+
+		$result = SMP_Sanitizer::sanitize( self::SECTION_COMMON_GENERAL, array( $key => $value ) );
+		$this->assertEquals( $expected, $result[ $key ] );
+	}
+
+	/**
+	 * Sanitize setting_close_popup_by_clicking_anywhere
+	 */
+	public function testCanBeSanitizedSettingClosePopupByClickingAnywhere(): void {
+		$key      = 'setting_close_popup_by_clicking_anywhere';
+		$value    = 3;
+		$expected = 1;
+
+		$result = SMP_Sanitizer::sanitize( self::SECTION_COMMON_GENERAL, array( $key => $value ) );
+		$this->assertEquals( $expected, $result[ $key ] );
+	}
+
+	/**
+	 * Sanitize setting_close_popup_when_esc_pressed
+	 */
+	public function testCanBeSanitizedSettingClosePopupWhenEscPressed(): void {
+		$key      = 'setting_close_popup_when_esc_pressed';
+		$value    = 4;
+		$expected = 1;
+
+		$result = SMP_Sanitizer::sanitize( self::SECTION_COMMON_GENERAL, array( $key => $value ) );
+		$this->assertEquals( $expected, $result[ $key ] );
+	}
+
+	/**
+	 * Sanitize setting_show_on_mobile_devices
+	 */
+	public function testCanBeSanitizedSettingShowOnMobileDevices(): void {
+		$key      = 'setting_show_on_mobile_devices';
+		$value    = 5;
+		$expected = 1;
+
+		$result = SMP_Sanitizer::sanitize( self::SECTION_COMMON_GENERAL, array( $key => $value ) );
+		$this->assertEquals( $expected, $result[ $key ] );
+	}
+
+	/**
+	 * Sanitize setting_show_admin_bar_menu
+	 */
+	public function testCanBeSanitizedSettingShowAdminBarMenu(): void {
+		$key      = 'setting_show_admin_bar_menu';
+		$value    = 6;
+		$expected = 1;
+
+		$result = SMP_Sanitizer::sanitize( self::SECTION_COMMON_GENERAL, array( $key => $value ) );
+		$this->assertEquals( $expected, $result[ $key ] );
 	}
 
 	/**
